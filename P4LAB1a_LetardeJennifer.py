6524846c2c63000646edef0ae4d28b7df4bc0191
letardej0661 <125038907+letardej0661@users.noreply.github.com>
2023-04-20 23:08:52 -0400

    Add files via upload

---
+++ P4LAB1a_LetardeJennifer.py
@@ -0,0 +1,15 @@
+import turtle
+
+for i in [0,1,2,3]:
+
+    turtle.forward(100)
+
+    turtle.left(90)
+
+turtle.left(45)
+
+turtle.forward(72)
+
+turtle.left(90)
+
+turtle.forward(72)
---
+++ P4LAB1b_LetardeJennifer.py
@@ -0,0 +1,24 @@
+import turtle
+wn = turtle.Screen()
+wn.bgcolor("orange")
+
+jenn = turtle.Turtle()
+jenn.color("blue")
+jenn.pensize(10)
+
+jenn.right(90)
+jenn.forward(50)
+jenn.left(90)
+jenn.forward(50)
+jenn.left(90)
+jenn.forward(150)
+jenn.right(90)
+
+jenn.penup()
+jenn.forward(25)
+jenn.pendown()
+
+jenn.right(90)
+jenn.forward(150)
+jenn.left(90)
+jenn.forward(50)
